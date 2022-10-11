38d1b721a1a1be53a859b9c5a3295e17714fef3c
dbrjs4594283 <dbrjs4594283@naver.com>
2022-10-11 14:59:14 +0900

    Ch1 중복 문자 제거(**)

---
+++ src/main/java/Ch1/Remove_Duplicate_Word.java
@@ -0,0 +1,21 @@
+// 10.11 복습
+package Ch1;
+
+import java.util.Scanner;
+
+public class Remove_Duplicate_Word {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String ans = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            if(i==str.indexOf(str.charAt(i))) {
+                ans+=str.charAt(i);
+            }
+
+        }
+        System.out.println(ans);
+
+    }
+}
