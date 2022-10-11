295a96a3523417c24961bdea08dce5eccffc858c
dbrjs4594283 <dbrjs4594283@naver.com>
2022-10-11 19:51:29 +0900

    Ch2 격자판 최대합

---
+++ src/main/java/Ch2/Grid_Maximum_Sum.java
@@ -0,0 +1,37 @@
+package Ch2;
+
+import java.util.Scanner;
+
+public class Grid_Maximum_Sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n][n];
+        for(int i = 0; i<n; i++) {
+            for(int j = 0; j<n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        int sum1, sum2;
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum1 = sum2 = 0;
+            for (int j = 0; j < n; j++) {
+                sum1 += arr[i][j];
+                sum2 += arr[j][i];
+            }
+            ans = Math.max(ans, sum1);
+            ans = Math.max(ans, sum2);
+        }
+        sum1 = sum2 = 0;
+        for (int i = 0; i < n; i++) {
+            sum1 += arr[i][i];
+            sum2 += arr[i][n - i - 1];
+        }
+        ans = Math.max(ans, sum1);
+        ans = Math.max(ans, sum2);
+        System.out.println(ans);
+    }
+}
