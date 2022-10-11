0bf7b874e7fa7d8eb53e41fbcdeae0e6b661d112
dbrjs4594283 <dbrjs4594283@naver.com>
2022-10-11 14:39:35 +0900

    Ch1 특정 단어 뒤집기(다시 풀기)

---
+++ src/main/java/Ch1/Reverse_Certain_Word.java
@@ -0,0 +1,31 @@
+// 10.11 복습(다시 풀어보기)
+package Ch1;
+
+import java.util.Scanner;
+
+public class Reverse_Certain_Word {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[] ans = sc.next().toCharArray();
+        int ri;
+        int li;
+        li = 0;
+        ri = ans.length - 1;
+
+        while (ri > li) {
+            if (!Character.isAlphabetic(ans[ri])) ri--;
+            else if (!Character.isAlphabetic(ans[li])) li++;
+            else {
+                char tmp = ans[ri];
+                ans[ri] = ans[li];
+                ans[li] = tmp;
+                ri--;
+                li++;
+            }
+
+        }
+        for(char x : ans) {
+            System.out.print(x);
+        }
+    }
+}
