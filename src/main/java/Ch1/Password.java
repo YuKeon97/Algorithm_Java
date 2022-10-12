c1ad54bb6bc8a659fb29a714026f3e9f62ae4b7d
dbrjs4594283 <dbrjs4594283@naver.com>
2022-10-12 22:26:13 +0900

    Ch1 암호(*****)

--- src/main/java/Ch1/Password.java
+++ src/main/java/Ch1/Password.java
@@ -6,17 +6,36 @@ public class Password {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        String str = sc.next();
         String ans = "";
-
+        String str = sc.next();
         for (int i = 0; i < n; i++) {
             String tmp = str.substring(0, 7).replace('#', '1').replace('*', '0');
             int num = Integer.parseInt(tmp, 2);
-            ans+=(char) num;
+            ans += (char) num;
             str = str.substring(7);
         }
         System.out.println(ans);
-
-
     }
 }
+
+
+//package Ch1;
+//
+//        import java.util.Scanner;
+//
+//public class Password {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        String str = sc.next();
+//        String ans = "";
+//
+//        for (int i = 0; i < n; i++) {
+//            String tmp = str.substring(0, 7).replace('#', '1').replace('*', '0');
+//            int num = Integer.parseInt(tmp, 2);
+//            ans+=(char) num;
+//            str = str.substring(7);
+//        }
+//        System.out.println(ans);
+//    }
+//}
