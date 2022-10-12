745c76a0f5717afe58a6a1a4a78d237c047c2bc7
dbrjs4594283 <dbrjs4594283@naver.com>
2022-10-12 20:38:41 +0900

    Ch1 유효한 팰리드롬(****)

---
+++ src/main/java/Ch1/Vaild_Palindrome.java
@@ -0,0 +1,17 @@
+package Ch1;
+
+import java.util.Scanner;
+
+public class Vaild_Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine().toLowerCase().replaceAll("[^a-z]", "");
+        String tmp = new StringBuilder(str).reverse().toString();
+
+        if (str.equals(tmp)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
