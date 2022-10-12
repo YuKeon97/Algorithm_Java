9ea88ddf643a6f719c8fd02ba16f0cf6d0372cea
dbrjs4594283 <dbrjs4594283@naver.com>
2022-10-12 22:13:25 +0900

    Ch1 문자열 압축(****)

--- src/main/java/Ch1/Compressed_String.java
+++ src/main/java/Ch1/Compressed_String.java
@@ -5,23 +5,49 @@ import java.util.Scanner;
 public class Compressed_String {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = sc.next()+" ";
+        String str = sc.next() + " ";
         int cnt = 1;
         String ans = "";
-        for (int i = 0; i < str.length() - 1; i++) {
+
+        for (int i = 0; i < str.length()-1; i++) {
             if (str.charAt(i) == str.charAt(i + 1)) {
                 cnt++;
             } else {
+                cnt = 1;
                 ans += str.charAt(i);
-                if (cnt > 1) {
-                    ans += String.valueOf(cnt);
-                }
-                cnt=1;
+                ans += cnt;
             }
+            System.out.println(ans);
+
         }
-        System.out.println(ans);
     }
 }
+
+// 1번 풀이
+//package Ch1;
+//
+//        import java.util.Scanner;
+//
+//public class Compressed_String {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String str = sc.next()+" ";
+//        int cnt = 1;
+//        String ans = "";
+//        for (int i = 0; i < str.length() - 1; i++) {
+//            if (str.charAt(i) == str.charAt(i + 1)) {
+//                cnt++;
+//            } else {
+//                ans += str.charAt(i);
+//                if (cnt > 1) {
+//                    ans += String.valueOf(cnt);
+//                }
+//                cnt=1;
+//            }
+//        }
+//        System.out.println(ans);
+//    }
+//}
 // 2번 풀이
 //    Scanner sc = new Scanner(System.in);
 //    String str = sc.next();
