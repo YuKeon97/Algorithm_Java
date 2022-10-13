ac20894bbe43e9bfc4f7ec549528e6a71cb191ca
dbrjs4594283 <dbrjs4594283@naver.com>
2022-10-13 10:23:03 +0900

    Ch1 암호(*****)

--- src/main/java/Ch1/Password.java
+++ src/main/java/Ch1/Password.java
@@ -6,8 +6,9 @@ public class Password {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        String ans = "";
         String str = sc.next();
+        String ans = "";
+
         for (int i = 0; i < n; i++) {
             String tmp = str.substring(0, 7).replace('#', '1').replace('*', '0');
             int num = Integer.parseInt(tmp, 2);
