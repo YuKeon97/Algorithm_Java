25bbcdf767436f1049d6aa2fa1e2b27d0031348f
dbrjs4594283 <dbrjs4594283@naver.com>
2022-10-13 11:27:01 +0900

    Ch1 대소문자 변환(*)

--- src/main/java/Ch1/Revert_Upper_Lower_Case_2.java
+++ src/main/java/Ch1/Revert_Upper_Lower_Case_2.java
@@ -18,3 +18,24 @@ public class Revert_Upper_Lower_Case_2 {
         System.out.println(ans);
     }
 }
+
+//package Ch1;
+//
+//        import java.util.Scanner;
+//
+//public class Revert_Upper_Lower_Case_2 {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String str = sc.next();
+//        String ans = "";
+//
+//        for (int i = 0; i < str.length(); i++) {
+//            if (Character.isUpperCase(str.charAt(i))) {
+//                ans += Character.toLowerCase(str.charAt(i));
+//            } else {
+//                ans += Character.toUpperCase(str.charAt(i));
+//            }
+//        }
+//        System.out.println(ans);
+//    }
+//}
