09e209dec95f9cd96490e2f2dec3f9cf4e009cfd
dbrjs4594283 <dbrjs4594283@naver.com>
2022-10-13 11:30:25 +0900

    Ch1 문장 찾기(*)

--- src/main/java/Ch1/Searching_String_1.java
+++ src/main/java/Ch1/Searching_String_1.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Searching_String_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         String str = sc.nextLine().toLowerCase();
         char chr = sc.nextLine().toLowerCase().charAt(0);
         int count = 0;
