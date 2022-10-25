382d562e63ade408262a4eba2e943ec2f9fa53d2
dbrjs4594283 <dbrjs4594283@naver.com>
2022-10-25 12:33:28 +0900

    Ch1 임시 반장 정하기(****)

--- src/main/java/Ch2/Decide_Interim_Leader_11.java
+++ src/main/java/Ch2/Decide_Interim_Leader_11.java
@@ -17,7 +17,7 @@ public class Decide_Interim_Leader_11 {
             }
         }
 
-        for (int i = 1; i <= n; i++) { // i : 행(학생) j : 열(학년)
+        for (int i = 1; i <= n; i++) {
             cnt = 0;
             for (int j = 1; j <= n; j++) {
                 for (int k = 1; k <= 5; k++) {
