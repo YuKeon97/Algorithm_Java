3f94a511a211c8a9b7a36a6e4334f73e97cb16ea
dbrjs4594283 <dbrjs4594283@naver.com>
2022-10-27 22:02:17 +0900

    Ch2 가위 바위 보(**)

--- src/main/java/Ch2/Rock_Paper_Scissors_3.java
+++ src/main/java/Ch2/Rock_Paper_Scissors_3.java
@@ -19,17 +19,11 @@ public class Rock_Paper_Scissors_3 {
         }
 
         for (int i = 0; i < n; i++) {
-            if (a[i] == b[i]) {
-                System.out.println("D");
-            } else if (a[i]==1 && b[i]==3) {
-                System.out.println("A");
-            } else if (a[i]==2 && b[i]==1) {
-                System.out.println("A");
-            } else if (a[i] == 3 && b[i] == 2) {
-                System.out.println("A");
-            } else {
-                System.out.println("B");
-            }
+            if (a[i] == b[i]) System.out.println("D");
+            else if (a[i]==1 && b[i]==3) System.out.println("A");
+            else if (a[i]==2 && b[i]==1) System.out.println("A");
+            else if (a[i]==3 && b[i]==2) System.out.println("A");
+            else System.out.println("B");
         }
     }
 }
