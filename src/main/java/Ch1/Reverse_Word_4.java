d4da44b0b0fde864d41ba1826441f4613ed396f8
dbrjs4594283 <dbrjs4594283@naver.com>
2022-10-31 11:28:54 +0900

    Ch1 단어 뒤집기(*)

--- src/main/java/Ch1/Reverse_Word_4.java
+++ src/main/java/Ch1/Reverse_Word_4.java
@@ -7,15 +7,10 @@ public class Reverse_Word_4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        String[] str = new String[n];
-
-        for (int i = 0; i < n; i++) {
-            str[i] = sc.next();
-        }
-
-        for (int i = 0; i < n; i++) {
-            StringBuilder ans = new StringBuilder(str[i]);
-            System.out.println(ans.reverse());
+        for(int i = 0; i<n; i++) {
+            String str = new StringBuilder(sc.next()).reverse().toString();
+            System.out.println(str);
         }
     }
 }
+
