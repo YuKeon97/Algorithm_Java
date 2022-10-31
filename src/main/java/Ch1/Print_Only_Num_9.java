4595dfec3346c5981891ebecec34ddcdabd7dd6f
dbrjs4594283 <dbrjs4594283@naver.com>
2022-10-31 11:23:55 +0900

    Ch1 숫자만 추출(*)

--- src/main/java/Ch1/Print_Only_Num_9.java
+++ src/main/java/Ch1/Print_Only_Num_9.java
@@ -4,15 +4,13 @@ import java.util.Scanner;
 public class Print_Only_Num_9 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
+        String str = sc.next();
         String ans = "";
 
-        for (int i = 0; i < str.length(); i++) {
-            if (Character.isDigit(str.charAt(i))) {
-                ans += str.charAt(i);
-            }
+        for(int i = 0; i<str.length(); i++) {
+            if(Character.isAlphabetic(str.charAt(i))) continue;
+            else ans += str.charAt(i);
         }
         System.out.println(Integer.parseInt(ans));
-
     }
 }
