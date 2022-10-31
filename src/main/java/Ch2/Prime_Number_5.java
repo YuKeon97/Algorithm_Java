ee2ad624716bff6d3623c44025ff341b745e154e
dbrjs4594283 <dbrjs4594283@naver.com>
2022-10-31 21:40:01 +0900

    Ch2 소수(*)

--- src/main/java/Ch2/Prime_Number_5.java
+++ src/main/java/Ch2/Prime_Number_5.java
@@ -6,20 +6,22 @@ public class Prime_Number_5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] arr = new int[n];
+        int[] arr = new int[n+1];
         int cnt = 0;
-        for (int i = 2; i < n; i++) {
-            if (arr[i] == 0) {
+
+        for(int i = 2; i<=n; i++) {
+            if(arr[i]==0) {
                 cnt++;
-                for (int j = i; j < n; j+=i) {
-                    arr[j] = 1;
+                for(int j = i; j<=n; j+=i) {
+                    arr[j] = i;
                 }
             }
         }
         System.out.println(cnt);
-
     }
 }
+
+
 
 
 
