b8f3edba7a530cf18117ecd7e79627f5b10b6bf2
dbrjs4594283 <dbrjs4594283@naver.com>
2022-11-01 23:33:21 +0900

    Ch4 아나그램

---
+++ src/main/java/Ch4/Anagram_2.java
@@ -0,0 +1,56 @@
+package Ch4;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Anagram_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        String str1 = sc.next();
+        String str2 = sc.next();
+
+        for(int i = 0; i<str1.length(); i++) {
+            map.put(str1.charAt(i), map.getOrDefault(str1.charAt(i), 0)+1);
+        }
+
+        boolean flag = true;
+        for(char key : str2.toCharArray()) {
+            if(!map.containsKey(key) || map.get(key) == 0) {
+                flag = false;
+                break;
+            }
+            map.put(key, map.get(key)-1);
+        }
+        if(flag) System.out.println("YES");
+        else System.out.println("NO");
+    }
+}
+
+/**HashMap 두 개*/
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        HashMap<Character, Integer> map1 = new HashMap<>();
+//        HashMap<Character, Integer> map2 = new HashMap<>();
+//
+//        String str1 = sc.next();
+//        String str2 = sc.next();
+//
+//        for(int i = 0; i<str1.length(); i++) {
+//            map1.put(str1.charAt(i), map1.getOrDefault(str1.charAt(i), 0)+1);
+//        }
+//
+//        for(int i = 0; i<str2.length(); i++) {
+//            map2.put(str2.charAt(i), map2.getOrDefault(str2.charAt(i), 0)+1);
+//        }
+//
+//        boolean flag = true;
+//
+//        for(char key : map1.keySet()) {
+//            if(!map2.containsKey(key)) flag = false;
+//            if(!map2.containsValue(map1.get(key))) flag = false;
+//        }
+//        if(flag) System.out.println("YES");
+//        else System.out.println("NO");
+//    }
