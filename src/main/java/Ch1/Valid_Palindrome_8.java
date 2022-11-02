8b6d6b3c9ce618c5b91aa8195ccc7591786a76a9
dbrjs4594283 <dbrjs4594283@naver.com>
2022-11-03 01:59:39 +0900

    Ch1 유효한 팰린드롬(*)

--- src/main/java/Ch1/Valid_Palindrome_8.java
+++ src/main/java/Ch1/Valid_Palindrome_8.java
@@ -2,13 +2,14 @@ package Ch1;
 
 import java.util.Scanner;
 
-public class Vaild_Palindrome_8 {
+public class Valid_Palindrome_8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine().toLowerCase().replaceAll("[^a-z]", "");
         String tmp = new StringBuilder(str).reverse().toString();
-        if(str.equals(tmp)) System.out.println("YES");
+        if(tmp.equals(str)) System.out.println("YES");
         else System.out.println("NO");
     }
 }
+
 
