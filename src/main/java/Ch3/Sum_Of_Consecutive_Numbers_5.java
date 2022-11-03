bb50d6950e24aab55a7ce1b56aa468c7c747e9cc
dbrjs4594283 <dbrjs4594283@naver.com>
2022-11-03 15:15:21 +0900

    Ch3 연속된 자연수의 합(****)

--- src/main/java/Ch3/Sum_Of_Consecutive_Numbers_5.java
+++ src/main/java/Ch3/Sum_Of_Consecutive_Numbers_5.java
@@ -6,28 +6,23 @@ public class Sum_Of_Consecutive_Numbers_5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int m = n / 2 + 1;
-        int[] arr = new int[m];
-        int sum = 0;
-        int lt = 0, rt = 0;
-        int cnt = 0;
+        int[] arr = new int[n/2+1];
+        int lp = 0, rp = 0;
+        int sum = 0, cnt = 0;
 
-        for (int i = 0; i < m; i++) {
-            arr[i] = i + 1;
-        }
+        for(int i = 0; i<arr.length; i++) arr[i] = i+1;
 
-        for (int i = 0; i < m; i++) {
-            sum += arr[rt++];
-            if (sum == n) cnt++;
-            while (sum >= n) {
-                sum -= arr[lt++];
-                if (sum == n) {
-                    cnt++;
-                }
-
+        for(int i = 0; i<n/2+1; i++) {
+            sum+=arr[rp++];
+            if(sum==n) cnt++;
+            while(sum>=n) {
+                sum-=arr[lp++];
+                if(sum==n)cnt++;
             }
         }
+
         System.out.println(cnt);
 
     }
 }
+
