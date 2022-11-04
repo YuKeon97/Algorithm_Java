02bd2e7844af1e4ab23a624e7b65a2e4fed538c3
dbrjs4594283 <dbrjs4594283@naver.com>
2022-11-04 13:47:35 +0900

    Ch2 임시반장 정하기(***) //break 이유

--- src/main/java/Ch2/Decide_Interim_Leader_11.java
+++ src/main/java/Ch2/Decide_Interim_Leader_11.java
@@ -6,34 +6,30 @@ public class Decide_Interim_Leader_11 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int cnt = 0, max = 0, ans = 0;
         int[][] arr = new int[n+1][6];
-        int cnt = 0;
-        int max = 0;
-        int ans = 0;
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j < 6; j++) {
+        for(int i = 1; i<=n; i++) {
+            for(int j = 1; j<6; j++) {
                 arr[i][j] = sc.nextInt();
             }
         }
-
-        for (int i = 1; i <= n; i++) {
+        for(int i = 1; i<=n; i++) {
             cnt = 0;
-            for (int j = 1; j <= n; j++) {
-                for (int k = 1; k <= 5; k++) {
-                    if (arr[i][k] == arr[j][k]) {
+            for(int j = 1; j<=n; j++) {
+                for(int k = 1; k<=5; k++) {
+                    if(arr[i][k]==arr[j][k]) {
                         cnt++;
                         break;
                     }
                 }
-            }
-            if (cnt > max) {
-                max = cnt;
-                ans = i;
+                if(cnt>max) {
+                    max = cnt;
+                    ans = i;
+                }
             }
         }
         System.out.println(ans);
+
     }
 }
-
 
