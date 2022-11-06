16384b45221d1768a30e5637b49fc277e6d02e4b
dbrjs4594283 <dbrjs4594283@naver.com>
2022-11-06 19:29:03 +0900

    Ch2 임시반장 정하기(**)

--- src/main/java/Ch2/Decide_Interim_Leader_11.java
+++ src/main/java/Ch2/Decide_Interim_Leader_11.java
@@ -8,18 +8,20 @@ public class Decide_Interim_Leader_11 {
         int n = sc.nextInt();
         int cnt = 0, max = 0, ans = 0;
         int[][] arr = new int[n+1][6];
+
         for(int i = 1; i<=n; i++) {
-            for(int j = 1; j<6; j++) {
+            for(int j = 1; j<=5; j++) {
                 arr[i][j] = sc.nextInt();
             }
         }
+
         for(int i = 1; i<=n; i++) {
             cnt = 0;
             for(int j = 1; j<=n; j++) {
                 for(int k = 1; k<=5; k++) {
                     if(arr[i][k]==arr[j][k]) {
                         cnt++;
-                        break;
+                        break; //cnt(같은 반이었던 횟수)가 아닌 학생(i)을 출력하는 것이기 때문에 break// 3번 4번 학생이 같은 반이었던 적이 있다가 중요하지 몇 번 같은 반이었던지를 출력하는 것이 아남
                     }
                 }
                 if(cnt>max) {
@@ -27,9 +29,9 @@ public class Decide_Interim_Leader_11 {
                     ans = i;
                 }
             }
+
         }
         System.out.println(ans);
-
     }
 }
 
