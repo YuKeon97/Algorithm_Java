1e32bb1240eff2f6a80cf87bafc6a5d2e720d980
dbrjs4594283 <dbrjs4594283@naver.com>
2022-11-10 16:01:36 +0900

    Ch3 최대 길이 연속부분수열(****)

--- src/main/java/Ch3/Max_Length_Continuous_Subsequence_6.java
+++ src/main/java/Ch3/Max_Length_Continuous_Subsequence_6.java
@@ -6,20 +6,22 @@ public class Max_Length_Continuous_Subsequence_6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int k = sc.nextInt();
+        int m = sc.nextInt();
         int[] arr = new int[n];
-        int lt = 0, cnt = 0, ans = 0;
-        for(int i = 0; i<n; i++) arr[i] = sc.nextInt();
+        int lp = 0, cnt = 0, ans = 0;
 
-        for(int rt = 0; rt<n; rt++) {
-            if(arr[rt]==0) cnt++;
-            while(cnt>k) {
-                if(arr[lt]==0) cnt--;
-                lt++;
+        for(int i = 0; i<n; i++) {arr[i] = sc.nextInt();}
+
+        for(int rp = 0; rp<n; rp++) {
+            if(arr[rp]==0) cnt++;
+            while(cnt>m) {
+                if(arr[lp]==0) cnt--;
+                lp++;
             }
-            ans = Math.max(ans, rt-lt+1);
+            ans = Math.max(ans, rp-lp+1);
         }
         System.out.println(ans);
     }
 }
+
 
