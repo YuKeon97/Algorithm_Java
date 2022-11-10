43c4bf409bdcd8f673f59626ae41c51d62056a12
dbrjs4594283 <dbrjs4594283@naver.com>
2022-11-10 15:04:42 +0900

    Ch3 연속 부분수열(***)

--- src/main/java/Ch3/Continuous_Subsequence_4.java
+++ src/main/java/Ch3/Continuous_Subsequence_4.java
@@ -6,20 +6,20 @@ public class Continuous_Subsequence_4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int k = sc.nextInt();
+        int m = sc.nextInt();
         int[] arr = new int[n];
         for(int i = 0; i<n; i++) arr[i] = sc.nextInt();
-        int cnt = 0;
-        int sum = 0;
-        int p1 = 0, p2 = 0;
-        while(p2<n) {
-            sum += arr[p2++];
-            if (sum==k) cnt++;
-            while(sum>=k) {
-                sum -= arr[p1++];
-                if(sum==k) cnt++;
+        int lt = 0, rt = 0, cnt = 0, sum = 0;
+
+        while(rt<arr.length) {
+            sum+=arr[rt++];
+            if(sum==m) cnt++;
+            while(sum>m) {
+                sum-=arr[lt++];
+                if(sum==m) cnt++;
             }
         }
         System.out.println(cnt);
     }
 }
+
