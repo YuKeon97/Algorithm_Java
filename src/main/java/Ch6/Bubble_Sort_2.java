81ce0552cb80d02e3e151431426581e4e34fe826
dbrjs4594283 <dbrjs4594283@naver.com>
2022-11-18 00:31:57 +0900

    Ch6 버블 정렬(****)

--- src/main/java/Ch6/Bubble_Sort_2.java
+++ src/main/java/Ch6/Bubble_Sort_2.java
@@ -9,19 +9,16 @@ public class Bubble_Sort_2 {
         int[] arr = new int[n];
         for(int i = 0; i<n; i++) arr[i] = sc.nextInt();
 
-
-        for(int i = 0; i<n-1; i++) {
-            for(int j = 0; j<n-i-1; j++) {
-                if(arr[j]>arr[j+1]) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
                     int tmp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = tmp;
                 }
             }
-
         }
-        for(int x : arr) System.out.print(x+" ");
-
+        for (int i : arr) System.out.print("i ");
     }
 }
 
