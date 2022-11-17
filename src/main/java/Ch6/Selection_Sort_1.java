145d6ea0c6329c9956ce31ecee92c1baa1239df3
dbrjs4594283 <dbrjs4594283@naver.com>
2022-11-17 23:07:53 +0900

    Ch6 선택 정렬(****)

--- src/main/java/Ch6/Selection_Sort_1.java
+++ src/main/java/Ch6/Selection_Sort_1.java
@@ -10,16 +10,17 @@ public class Selection_Sort_1 {
         for(int i = 0; i<n; i++) arr[i] = sc.nextInt();
 
 
-        for(int i = 0; i<n-1; i++) {
-            int idx = i;
-            for(int j = i+1; j<n; j++) {
-                if(arr[j]<arr[idx]) idx=j;
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+            for (int j = i+1; j < n; j++) {
+                if(arr[min]>arr[j]) min = j;
             }
             int tmp = arr[i];
-            arr[i] = arr[idx];
-            arr[idx] = tmp;
+            arr[i] = arr[min];
+            arr[min] = tmp;
         }
-        for(int x : arr) System.out.print(x+" ");
+
+        for (int i : arr) System.out.print(i+" ");
 
     }
 }
