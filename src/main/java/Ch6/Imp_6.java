a0238e5eeda3c7be45f0cddc12ad08a8da63d1fc
dbrjs4594283 <dbrjs4594283@naver.com>
2022-11-28 23:14:42 +0900

    Ch6 장난꾸러기

---
+++ src/main/java/Ch6/Imp_6.java
@@ -0,0 +1,21 @@
+package Ch6;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Imp_6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+        int[] tmp = arr.clone();
+
+        Arrays.sort(tmp);
+
+        for (int i = 0; i < n; i++) {
+            if (tmp[i] != arr[i]) System.out.print(i+1+" ");
+        }
+    }
+}
