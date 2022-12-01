39c5a1de39d53296dfc2055aaa773a4c33210468
dbrjs4594283 <dbrjs4594283@naver.com>
2022-12-02 01:48:40 +0900

    Ch.7 부분집합 찾기

---
+++ src/main/java/Ch7/Subset_DFS_6.java
@@ -0,0 +1,27 @@
+package Ch7;
+
+public class Subset_DFS_6 {
+    static int n;
+    static int[] ch;
+
+    public void DFS(int L) {
+        if (L == n + 1) {
+            String tmp = " ";
+            for (int i = 1; i <= n; i++) if(ch[i]==1) tmp += (i + " ");
+            if (tmp.length()>0) System.out.println(tmp);
+        } else {
+            ch[L] = 1;
+            DFS(L + 1);
+            ch[L] = 0;
+            DFS(L + 1);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Subset_DFS_6 t = new Subset_DFS_6();
+        n=3;
+        ch = new int[n + 1];
+        t.DFS(1);
+    }
+}
