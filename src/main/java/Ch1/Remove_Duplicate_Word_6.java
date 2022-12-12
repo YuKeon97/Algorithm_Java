c6d5c119dc43a79afd3c1c4f85bf25710c21a365
dbrjs4594283 <dbrjs4594283@naver.com>
2022-12-12 22:21:55 +0900

    1

--- src/main/java/Ch1/Remove_Duplicate_Word_6.java
+++ src/main/java/Ch1/Remove_Duplicate_Word_6.java
@@ -8,14 +8,11 @@ public class Remove_Duplicate_Word_6 {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
         String ans = "";
-
         for (int i = 0; i < str.length(); i++) {
             if(i==str.indexOf(str.charAt(i))) {
                 ans+=str.charAt(i);
             }
-
         }
-
         System.out.println(ans);
     }
 }
