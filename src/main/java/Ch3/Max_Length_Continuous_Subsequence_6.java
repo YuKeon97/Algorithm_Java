80aab655795844820631afa83e62df7137ba2c4c
dbrjs4594283 <dbrjs4594283@naver.com>
2022-12-25 22:23:04 +0900

    1

--- src/main/java/Ch3/Max_Length_Continuous_Subsequence_6.java
+++ src/main/java/Ch3/Max_Length_Continuous_Subsequence_6.java
@@ -9,9 +9,7 @@ public class Max_Length_Continuous_Subsequence_6 {
         int m = sc.nextInt();
         int[] arr = new int[n];
         int lp = 0, cnt = 0, ans = 0;
-
         for(int i = 0; i<n; i++) {arr[i] = sc.nextInt();}
-
         for(int rp = 0; rp<n; rp++) {
             if(arr[rp]==0) cnt++;
             while(cnt>m) {
